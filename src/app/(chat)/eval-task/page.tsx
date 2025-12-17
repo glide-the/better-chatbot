fe64f2c5310cc6507cc8be9acd17e452d1c4aefc
glide-the <dmeck@suoxya.com>
2025-12-17 17:36:12 +0800

    feat: align eval task chat behavior

--- src/app/(chat)/eval-task/page.tsx
+++ src/app/(chat)/eval-task/page.tsx
@@ -1,20 +1,20 @@
 import { EvalTaskChatBot } from "@/components/eval-task-chat-bot";
-import { generateUUID } from "lib/utils";
 import { getSession } from "auth/server";
+import { generateUUID } from "lib/utils";
 import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function EvalTaskPage() {
   const session = await getSession();
-  if (!session) {
-    redirect("/sign-in");
+
+  if (!session?.user) {
+    redirect("/login");
   }
-  const id = generateUUID();
+
+  const threadId = generateUUID();
 
   return (
-    <div className="flex h-full w-full flex-col">
-      <EvalTaskChatBot initialMessages={[]} threadId={id} key={id} />
-    </div>
+    <EvalTaskChatBot key={threadId} threadId={threadId} initialMessages={[]} />
   );
 }
